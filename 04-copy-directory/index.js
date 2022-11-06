3567a6e10b627315593e06bb7190789e72258476
Nikita <nikitosminsk100@gmail.com>
2022-11-06 16:58:19 +0300

    feat: 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,34 @@
+const fs = require('fs')
+const path = require('path')
 
 
+const copy = async (srcDir, dstDir) => {
+    const files = await fs.promises.readdir(srcDir).then(data => data)
 
-const main = () => {
+    const promises = files.map(file => new Promise((resolve, reject) => {
+        const src = srcDir + '/' + file
+        const dst = dstDir + '/' + file
 
+        try {
+            fs.promises.writeFile(dst, fs.readFileSync(src))
+        } catch (e) {
+            console.log('could\'t copy file: ' + dst + 'Error: ' + e)
+        }
+
+        resolve()
+    }))
+
+    return await Promise.all(promises).then(result => result)
 }
+
+const main = async () => {
+    const source = path.join(__dirname, 'files')
+    const destination = path.join(__dirname, 'files-copy')
+
+    await fs.promises.rm(destination, {recursive: true, force: true})
+    await fs.promises.mkdir(destination)
+
+    await copy(source, destination)
+}
+
+main()
